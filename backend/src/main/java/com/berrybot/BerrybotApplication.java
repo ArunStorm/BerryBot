c8e715b682915c3d1d2a86dc53c1bdea18ab2341
Arunkumar <arunkumar19962000@gmail.com>
2026-08-17 07:27:45 +0530

    feat: initialize BerryBot foundation
    Initialize BerryBot full-stack foundation

---
+++ backend/src/main/java/com/berrybot/BerrybotApplication.java
@@ -0,0 +1,12 @@
+package com.berrybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerrybotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BerrybotApplication.class, args);
+    }
+}
